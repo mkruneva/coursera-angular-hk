0fdf7351a6be6dd95b40a797fcbe9a28ef0f0456
mkruneva <marta.kruneva@gmail.com>
2017-10-26 19:56:13 +0100

    app with ui-router, DishdetailsController

--- confusion/app/scripts/controllers.js
+++ confusion/app/scripts/controllers.js
@@ -1,46 +1,54 @@
 'use strict';
 
 angular.module('confusionApp', [])
-.controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
-    $scope.tab = 1;
-    $scope.dishName = '';
-    $scope.filtText = '';
-    $scope.sortWords = [];
-    $scope.showDishDetails = false;
+    .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
-    $scope.dishes = menuFactory.getDishes();
+        $scope.returnDish = function(index) {
+            console.log(menuFactory.getDish(index));
+            return menuFactory.getDish(index);
+        };
 
-     $scope.returnDish = function(index) {
-        console.log(menuFactory.getDish(index));
-        return menuFactory.getDish(index); 
-    };
-    
-    $scope.toggleDetails = function () {
-        $scope.showDishDetails = !$scope.showDishDetails;
-    };
+    }])
 
-    $scope.select = function(setTab) {
-        this.tab = setTab;
-        switch (setTab) {
-            case 2:
-                this.filtText = 'appetizer';
-                break;
-            case 3:
-                this.filtText = 'mains';
-                break;
-            case 4:
-                this.filtText = 'dessert';
-                break;
-            default:
-                this.filtText = '';
-        }
-    };
 
-    $scope.isSelected = function(checkTab) {
-        return (this.tab === checkTab);
-    };
+    .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
-   
+        $scope.tab = 1;
+        $scope.dishName = '';
+        $scope.filtText = '';
+        $scope.sortWords = [];
+        $scope.showDishDetails = false;
 
-}]);
+        $scope.dishes = menuFactory.getDishes();
+
+        $scope.returnDish = function(index) {
+            console.log(menuFactory.getDish(index));
+            return menuFactory.getDish(index);
+        };
+
+        $scope.toggleDetails = function() {
+            $scope.showDishDetails = !$scope.showDishDetails;
+        };
+
+        $scope.select = function(setTab) {
+            this.tab = setTab;
+            switch (setTab) {
+                case 2:
+                    this.filtText = 'appetizer';
+                    break;
+                case 3:
+                    this.filtText = 'mains';
+                    break;
+                case 4:
+                    this.filtText = 'dessert';
+                    break;
+                default:
+                    this.filtText = '';
+            }
+        };
+
+        $scope.isSelected = function(checkTab) {
+            return (this.tab === checkTab);
+        };
+    }]);
