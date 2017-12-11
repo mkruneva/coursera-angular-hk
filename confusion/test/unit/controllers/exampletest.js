91f7ddd69e3c593c18675e976c79cc157d1855df
mkruneva <marta.kruneva@gmail.com>
2017-12-11 18:27:09 +0000

    unify angular and ng dependancies versions

--- confusion/test/unit/controllers/exampletest.js
+++ confusion/test/unit/controllers/exampletest.js
@@ -1,5 +1,15 @@
 describe('example test', function() {
-  it('should be true', function() {
-    expect('foo').toBe('foo');
-  });
+    it('should be true', function() {
+        expect('foo').toBe('foo');
+    });
+});
+
+
+describe('App test', function() {
+    beforeEach(module('confusionApp'));
+    describe('always true', function() {
+        it('should be true', function() {
+            expect('moo').toBe('moo');
+        });
+    });
 });
